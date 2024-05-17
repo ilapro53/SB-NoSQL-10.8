a8c3b602fe7fbc117a9dc3bda12d33b0f421c3ea
Ilya Pro <ilapro53@gmail.com>
2024-05-17 22:49:34 +0300

    "Connection refused" error fixed

--- stream_processor.py
+++ stream_processor.py
@@ -1,35 +1,38 @@
 # Реализуйте код обработки результата MapReduce
 import sys, json
 
-ORDERED_BY_ID = False
-
 uniq_tracks_count = 0
+uniq_tracks = dict()
 popular_tracks_count = 0
+
 top_2_tracks = [None, None]
-uniq_tracks = set()
 
 
 for line in sys.stdin:
-    # line - результат работы MapReduce, финальный формат от reducer.py - <track_id>\t<N>
+    # Парсинг результата работы MapReduce (финальный формат от reducer.py - <track_id>\t<N>)
     track_id, count = line.strip().split('\t')
     count = int(count)
     
-    uniq_tracks_count += 1
+    # Подсчет уникальных треков
+    if not (track_id in uniq_tracks):
+        uniq_tracks[track_id] = 0
+    count_before = uniq_tracks[track_id]
+    uniq_tracks[track_id] += count
     
+    # Подсчет популярных треков
+    if (uniq_tracks[track_id] > 20) and (count_before <= 20):
+        popular_tracks_count += 1
+    
+    # Топ 2 трека
+    total_count = uniq_tracks[track_id]
     if top_2_tracks[0] is None:
-        top_2_tracks[0] = track_id, count
-    elif (top_2_tracks[1] is None) and (top_2_tracks[0][1] >= count):
-        top_2_tracks[1] = track_id, count
-    elif top_2_tracks[0][1] < count:
+        top_2_tracks[0] = track_id, total_count
+    elif (top_2_tracks[1] is None) and (top_2_tracks[0][1] >= total_count):
+        top_2_tracks[1] = track_id, total_count
+    elif top_2_tracks[0][1] < total_count:
         top_2_tracks[1] = top_2_tracks[0]
-        top_2_tracks[0] = track_id, count
-    
-    if count > 20:
-        if ORDERED_BY_ID:
-            popular_tracks_count += 1
-        else:
-            uniq_tracks.add(track_id)
-
+        top_2_tracks[0] = track_id, total_count
+        
 uniq_tracks_count = len(uniq_tracks)
 
 data = {
